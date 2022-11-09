909f0b25b580b74dc4cbccd587111ff49483f91d
lenny79 <117748111+lenny79@users.noreply.github.com>
2022-11-09 15:56:36 +0100

    Update random_range.py

--- random_range.py
+++ random_range.py
@@ -2,6 +2,7 @@ from random import randrange
 
 def rndrange(event, context):
   rnd_num = 0
+  par = event['params']
   
   try:
     a, b = int(event['params']['a']), int(event['params']['b'])
@@ -10,5 +11,5 @@ def rndrange(event, context):
   except Exception:
     a, b = 0, 0
       
-  returnstr = "Output: " + str(rnd_num) + " / First Argument: " + str(event) + " / Second Argument: " + str(context) + " / a: " + str(a) + " / b: " + str(b)
+  returnstr = "Output: " + str(rnd_num) + " / First Argument: " + str(event) + " / Second Argument: " + str(context) + " / a: " + str(a) + " / b: " + str(b) + " / params: " + str(params)
   return returnstr
