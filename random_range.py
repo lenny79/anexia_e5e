044bf9f7b8dae44c445b2509469d12f2778c6c68
lenny79 <117748111+lenny79@users.noreply.github.com>
2022-11-09 09:39:40 +0100

    Update random_range.py

--- random_range.py
+++ random_range.py
@@ -1,8 +1,6 @@
 from random import randrange
 
 def rndrange(a, b):
-  in_a = int(a)
-  in_b = int(b)
   rnd_num = randrange(1, 7)
   returnstr = "Out: " + rnd_num + " // " + a + b
   return returnstr
