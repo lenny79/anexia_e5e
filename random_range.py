35929499aef0a6111da3a599ace30fdec924fce8
lenny79 <117748111+lenny79@users.noreply.github.com>
2022-11-09 09:49:41 +0100

    Update random_range.py

--- random_range.py
+++ random_range.py
@@ -2,5 +2,5 @@ from random import randrange
 
 def rndrange(a, b):
   rnd_num = randrange(1, 7)
-  returnstr = "Out: " + str(rnd_num) + " // " + str(a) + str(b)
+  returnstr = "Output: " + str(rnd_num) + "\n First Argument: " + str(a) + "\n Second Argument: " + str(b)
   return returnstr
