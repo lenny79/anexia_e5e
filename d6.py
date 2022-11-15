2481d9eb26303b37a0b7d144d34d41987f05c3dd
lenny79 <117748111+lenny79@users.noreply.github.com>
2022-11-15 14:00:45 +0100

    Update and rename d6.py to dice.py

--- d6.py
+++
@@ -1,5 +0,0 @@
-from random import randrange
-
-def rollD6(event, context):
-  rnd_num = randrange(1, 7)
-  return rnd_num
---
+++ dice.py
@@ -0,0 +1,9 @@
+from random import randrange
+
+def rollD6(event, context):
+  rnd_d6 = randrange(1, 7)
+  return rnd_num
+
+def rolld20(event, context):
+  rnd_d20 = randrange(1,21)
+  return rnd_d20
